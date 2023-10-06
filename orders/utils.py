8f3470c95387f5223731361f249bc2b6169a7290
mxstrv <37843972+mxstrv@users.noreply.github.com>
2023-10-06 20:53:56 +0300

    chore: add docstring

--- orders/utils.py
+++ orders/utils.py
@@ -2,7 +2,19 @@ from django.core.mail import send_mail
 from django.conf import settings
 
 
-def send_order_emails(model, version, maillist):
+def send_order_emails(model, version, mail_list):
+    """
+   Функция отправляет электронные письма клиентам, которые находятся
+   в листе ожидания.
+
+   :param model: Название модели товара.
+   :type model: str
+   :param version: Версия товара.
+   :type version: str
+   :param mail_list: Список адресов электронной почты, на которые будет отправлено уведомление.
+   :type mail_list: list
+   :return: None
+    """
     message_text = f'Добрый день!\n' \
                    f'Недавно вы интересовались ' \
                    f'нашим роботом модели {model}, версии {version}.\n' \
@@ -13,6 +25,6 @@ def send_order_emails(model, version, maillist):
         subject='Робот в наличии',
         message=message_text,
         from_email=settings.EMAIL_HOST_USER,
-        recipient_list=maillist,
+        recipient_list=mail_list,
         fail_silently=False
     )
