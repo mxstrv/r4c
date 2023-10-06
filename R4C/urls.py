6b90a26aa77e42ac9b17dccfc3834fa90389434b
mxstrv <37843972+mxstrv@users.noreply.github.com>
2023-10-06 16:52:44 +0300

    feat: add excel url

--- R4C/urls.py
+++ R4C/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path, include
 
 urlpatterns = [
-    path('api/', include('api.urls'))
+    path('api/', include('api.urls')),
+    path('excel/', include('orders.urls'))
 ]
