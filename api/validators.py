ef60492b8b298be65a81b23b104bbbd35fbe3f91
mxstrv <37843972+mxstrv@users.noreply.github.com>
2023-10-06 15:51:37 +0300

    feat: add regex validation

--- api/validators.py
+++ api/validators.py
@@ -1,9 +1,11 @@
+import re
 from datetime import datetime
 
 
 def validate_robot_creation(data: dict) -> tuple[bool, dict]:
     """
     Функция валидирует данные о роботе, передаваемые на вход.
+    Валидация модели и версии идет с использованием регулярных выражений.
 
     :param data: Словарь с данными о роботе, включающий в себя модель, версию и дату создания.
     :type data: dict
@@ -20,13 +22,15 @@ def validate_robot_creation(data: dict) -> tuple[bool, dict]:
 
     data = {key: data[key] for key in fields}
 
-    if len(data['model']) > 2 or len(data['version']) > 2:
+    regex_pattern = r"^[A-Z0-9]{2}$"
+
+    if not re.match(regex_pattern, data['model']) or not re.match(regex_pattern, data['version']):
         return False, {'error': 'model and/or version should be 2 characters max.'}
 
     date_required_format = '%Y-%m-%d %H:%M:%S'
     try:
         datetime.strptime(data['created'], date_required_format)
     except ValueError:
-        return False, {'error': "Incorrect data format, should be YYYY-MM-DD HH:MM:SS"}
+        return False, {'error': "data format should be YYYY-MM-DD HH:MM:SS"}
 
     return True, data
