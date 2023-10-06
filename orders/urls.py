f1da4cced8f1917fa6973ca6e9fa564432a9b1a6
mxstrv <37843972+mxstrv@users.noreply.github.com>
2023-10-06 17:07:31 +0300

    chore: refactor file structure

--- orders/urls.py
+++ orders/urls.py
@@ -1,7 +1,7 @@
-from django.urls import path
-
-from orders.views import download_excel_file
-
-urlpatterns = [
-    path('download/', download_excel_file, name='download_excel')
-]
+# from django.urls import path
+#
+# from orders.views import download_excel_file
+#
+# urlpatterns = [
+#     path('download/', download_excel_file, name='download_excel')
+# ]
